79b1be4779a64692809d9ed35e4af876f2dcde5c
MinSun <hsun0987@daum.net>
2023-10-25 12:57:48 +0900

    5th - modulo() 작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,7 +58,7 @@ public class Main {
        System.out.println(a + " / " + b + " = " + (a / b));
     }
     private static void modulo(int a, int b) {
-       // System.out.println(a + " % " + b + " = " + (a % b));
+       System.out.println(a + " % " + b + " = " + (a % b));
     }
 }
 
